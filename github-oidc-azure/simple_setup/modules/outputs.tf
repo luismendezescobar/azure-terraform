4a796104929c40e8268b7a52e693de6602768027
luismendezescobar <luismendezescobar@gmail.com>
2023-03-09 19:08:26 -0600

    first commit

---
+++ github-oidc-azure/simple_setup/modules/outputs.tf
@@ -0,0 +1,11 @@
+
+# Return the full service principal object
+output "service_principal" {
+  value = azuread_service_principal.oidc
+  description = "The full service principal object associated with the application."
+}
+
+output "azuread_application" {
+  value = azuread_application.oidc
+  description = "The full application object associated with the service principal."
+}
